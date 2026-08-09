355230c5c3eac8e679fcb92614ae4068de5a7989
Muhammad Zayeem <muhammadzayeem@hotmail.com>
2026-08-10 03:44:43 +0500

    Topic 02: Data Structures(List)

---
+++ Week 01/Topic 02/List.py
@@ -0,0 +1,16 @@
+Fruits = ["Apple", "Strawberry", "Cherry", "Date", "Banana"]
+print(Fruits)
+print(Fruits[-1])
+print(Fruits[-2])
+
+Fruits.append("Orange")  #Add item to end
+print(Fruits)
+Fruits.insert(2,"Mango") #Add item to index
+print(Fruits)
+Fruits.remove("Date")    #Remove an item
+print(Fruits)
+Fruits.pop()             #Remove from last
+print(Fruits)
+Fruits.sort()
+print(Fruits)
+
